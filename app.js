59ce015858e7f0b85a2ac73a5643634048f5a0bc
brenley-woodard <brenleyew@gmail.com>
2022-10-25 15:19:28 -0700

    event listener for button click working

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const nameDisplay = document.getElementById('name');
 console.log('nameDisplay', nameDisplay);
+
+nameDisplay.textContent = 'Spider';
+
+const button = document.getElementById('button');
+button.addEventListener('click', () => {
+    console.log('button clicked');
+});
