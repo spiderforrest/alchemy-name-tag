4a8a3891fabba6b4ba56fc7660cf9686b336392a
brenley-woodard <brenleyew@gmail.com>
2022-10-25 16:47:51 -0700

    clears input field, added some styling, need to finish background color

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const nameDisplay = document.getElementById('name');
 console.log('nameDisplay', nameDisplay);
 
-nameDisplay.textContent = 'Spider';
+nameDisplay.textContent = 'John Doe';
 
 const button = document.getElementById('button');
 button.addEventListener('click', () => {
     console.log('button clicked');
     const inputBox = document.getElementById('input-box');
     nameDisplay.textContent = inputBox.value;
+    inputBox.value = '';
 });
 
 const colorDropdown = document.getElementById('color-select');
@@ -19,4 +20,15 @@ colorDropdown.addEventListener('change', () => {
     nameDisplay.classList.remove('goldenrod');
 
     nameDisplay.classList.add(colorDropdown.value);
+});
+
+const backgroundDropdown = document.getElementById('background-select');
+backgroundDropdown.addEventListener('change', () => {
+    console.log('color selected: ' + backgroundDropdown.value);
+
+    nameDisplay.classList.remove('rebeccapurple');
+    nameDisplay.classList.remove('blue');
+    nameDisplay.classList.remove('goldenrod');
+
+    nameDisplay.classList.add(backgroundDropdown.value);
 });
